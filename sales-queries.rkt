4a893461ab10446f8fe97c93a4b898566c0a5525
Zach morris <zwmvq2@umkc.edu>
2024-03-10 17:20:00 -0500

    prompt-query

--- sales-queries.rkt
+++ sales-queries.rkt
@@ -2,14 +2,14 @@
 
 (require csv-reading)  ;https://docs.racket-lang.org/csv-reading/index.html
 
-;test function
-(define my-input
+
+(define (assignment-input-file)
   "Video Games Sales.csv")
 
 ;Precondition:   File must be a csv file, which matches the  default csv-reader specs
 ;Postcondition: returns a list of lists,  where each line in the file is a list containing an element for each data attribute.
 ;Numeric values are stored as numeric types everything else is strings
-(define (load-data filename)
+(define (load-data [filename assignment-input-file])
   (map list<string>->list<number/string>
  (csv->list (open-input-file filename))))
 
@@ -35,3 +35,29 @@
       (if (string->number string)
           (string->number string)
           string)))
+
+;Postcondition Prompts the user for a type of query, and then calls the child function which ensures the type is valid and prompts for specific criteria
+;Returns a list containing a valid query type and additionial user input
+(define (prompt-query)
+ (print "Enter Querry Type [Name, Date, Publisher, Region, Genre,Skip]: ")
+ (prompt-for-specifics-of-query(string-titlecase  (read-line))))
+
+;Sub function for prompt-query
+;Postcondition:  Prints a message relevant to the query type.  Then prompts the user for input,  and returns a list containing query type and user input
+;If query type is invalid, this function will call the parent prompt-query function again. This means that the recursion will not terminate until a valid query type is entered
+(define (prompt-for-specifics-of-query query-type)
+  (print (query-type->message query-type))
+  (if (equal? (query-type->message query-type) "INVALID QUERY TYPE!")
+      (prompt-query)
+      (list query-type (read-line))))
+
+(define (query-type->message query-type)
+   (define message-hash
+     (hash
+      "Name" "Enter Title: "
+      "Date" "Enter date-range [{year1}-{year2}]: "
+      "Publisher" "Enter publisher: "
+      "Region" "Enter Region [North-America,Europe,Japan,Rest of World,Global]: "
+      "Genre" "Enter Genre: "
+      "Skip" "Skipping search parameter. Input anything here,  it will be ignored"))
+   (hash-ref message-hash query-type "INVALID QUERY TYPE!"))
